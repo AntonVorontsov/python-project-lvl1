8a9aa4aef9ff2345a4c8bd047f96363636c70a28
Anton Vorontsov <antonvorontsov@Antons-MacBook-Pro.local>
2022-04-20 18:30:23 +0300

    correctin cli.py in the 3nd stage of the project

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,6 +4,6 @@ import prompt
 def welcome_user():
     """Get an user name and promt user."""
     name = prompt.string('May I have your name? ')
-    print('Hello, {0}!'.format(name))
+    print('Hello, ' + name + '!')
 
     
