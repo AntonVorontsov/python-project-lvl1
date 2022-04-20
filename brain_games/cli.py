e66b984a7e07925ab220bb4c553bdd816e818827
Anton Vorontsov <antonvorontsov@Antons-MacBook-Pro.local>
2022-04-20 18:14:47 +0300

    commiting the 3nd stage of the project

---
+++ brain_games/cli.py
@@ -0,0 +1,9 @@
+import prompt
+ 
+ 
+def welcome_user():
+    """Get an user name and promt user."""
+    name = prompt.string('May I have your name? ')
+    print('Hello, {0}!'.format(name))
+
+    
