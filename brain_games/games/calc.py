e90efce51e12c6243369dcb7c4179bc6b32a0588
Anton Vorontsov <antonvorontsov@Antons-MacBook-Pro.local>
2022-05-06 11:47:44 +0300

    6th stage not finished

---
+++ brain_games/games/calc.py
@@ -0,0 +1,17 @@
+import random
+
+game_task = 'What is the result of the expression?'
+
+def get_question_and_unswer():
+    num1 = random.randint(1, 10)
+    num2 = random.randint(1, 10)
+    operators = ["+", "-", "*"]
+    operator = random.choice(operators)
+    if operator == "+":
+        correct_answer = num1 + num2
+    elif operator == "-":
+        correct_answer = num1 - num2
+    else:
+        correct_answer = num1 * num2
+    question = f"{num1} {operator} {num2}"
+    return question, correct_answer
