779a9b3476ebd3d9a52dfa3d83fa8500cb6a799b
AntonVorontsov <vorontsovanton@gmail.com>
2022-05-11 16:24:39 +0300

    fixing mistakes review #1

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,10 +4,12 @@ GAME_TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def isprime(k):
-    if k==2 or k==3: return True
-    if k%2==0 or k<2: return False
-    for i in range(3, int(k**0.5)+1, 2):
-        if k%i==0:
+    if k == 2 or k == 3:
+        return True
+    if k % 2 == 0 or k < 2:
+        return False
+    for i in range(3, int(k ** 0.5) + 1, 2):
+        if k % i == 0:
             return False
     return True
 
